301c7b49c4e94ae3c1a641d3ec6b00d38f93643e
snllix <elev210714@elev.trondelagfylke.no>
2025-09-02 15:11:08 +0200

    commit

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+let name = "Sofiia"
+
+document.write("Hello" +  " " + name + "<br>")
+
+let tall = 1
+
+let tall1 = 9
+
+let tall2 = 7
+
+summ = tall1 + tall2
+summ1 = tall1 - tall2
+summ2 = tall1 / tall2
+summ3 = tall1 * tall2
+
+document.write(tall1 + "+" + tall2 + "=" + summ + "<br>")
+document.write(tall1 + "-" + tall2 + "=" + summ1 + "<br>")
+document.write(tall1 + "/" + tall2 + "=" + summ2 + "<br>")
+document.write(tall1 + "*" + tall2 + "=" + summ3 + "<br>")
+
+
+let alder = prompt()
+
+if(alder < 18){
+    document.write("Du får ikke kjøpe øl" + "<br>")
+}
+else{
+    document.write("Du får kjøpe øl")
+}
 
